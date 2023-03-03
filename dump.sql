76d1471c5e413ea72190708093d6790e5e42cd80
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-03 11:55:42 -0300

    09

--- dump.sql
+++ dump.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 14.4 (Debian 14.4-1.pgdg110+1)
 -- Dumped by pg_dump version 14.5
 
--- Started on 2023-03-03 14:40:34 UTC
+-- Started on 2023-03-03 14:54:45 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,7 +23,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 211 (class 1259 OID 35291)
+-- TOC entry 214 (class 1259 OID 35326)
 -- Name: sessions; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -37,7 +37,30 @@ CREATE TABLE public.sessions (
 
 
 --
--- TOC entry 210 (class 1259 OID 35284)
+-- TOC entry 213 (class 1259 OID 35325)
+-- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.sessions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3337 (class 0 OID 0)
+-- Dependencies: 213
+-- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
+
+
+--
+-- TOC entry 212 (class 1259 OID 35310)
 -- Name: shortens; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -52,7 +75,30 @@ CREATE TABLE public.shortens (
 
 
 --
--- TOC entry 209 (class 1259 OID 35278)
+-- TOC entry 211 (class 1259 OID 35309)
+-- Name: shortens_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.shortens_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3338 (class 0 OID 0)
+-- Dependencies: 211
+-- Name: shortens_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.shortens_id_seq OWNED BY public.shortens.id;
+
+
+--
+-- TOC entry 210 (class 1259 OID 35300)
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -65,7 +111,99 @@ CREATE TABLE public.users (
 );
 
 
--- Completed on 2023-03-03 14:40:34 UTC
+--
+-- TOC entry 209 (class 1259 OID 35299)
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3339 (class 0 OID 0)
+-- Dependencies: 209
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- TOC entry 3182 (class 2604 OID 35329)
+-- Name: sessions id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.sessions_id_seq'::regclass);
+
+
+--
+-- TOC entry 3179 (class 2604 OID 35313)
+-- Name: shortens id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.shortens ALTER COLUMN id SET DEFAULT nextval('public.shortens_id_seq'::regclass);
+
+
+--
+-- TOC entry 3177 (class 2604 OID 35303)
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- TOC entry 3190 (class 2606 OID 35335)
+-- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3188 (class 2606 OID 35319)
+-- Name: shortens shortens_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.shortens
+    ADD CONSTRAINT shortens_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3186 (class 2606 OID 35308)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3192 (class 2606 OID 35336)
+-- Name: sessions sessions_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
+--
+-- TOC entry 3191 (class 2606 OID 35320)
+-- Name: shortens shortens_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.shortens
+    ADD CONSTRAINT "shortens_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
+-- Completed on 2023-03-03 14:54:46 UTC
 
 --
 -- PostgreSQL database dump complete
